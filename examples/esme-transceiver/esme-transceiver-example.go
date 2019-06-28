e7a811ae0e3d8959bfa4c32d155ad5ca85ee0714
blorticus <boguese@hotmail.com>
2019-06-28 03:16:57 -0700

    Completely refactored esme/smsc-transceiver

--- examples/esme-transceiver/esme-transceiver-example.go
+++ examples/esme-transceiver/esme-transceiver-example.go
@@ -2,142 +2,198 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 	"net"
 	"os"
 	"path"
-	"path/filepath"
 	"smpp"
 	"strconv"
 )
 
-type Outputter struct {
+type outputter struct {
+	outputDebug bool
 }
 
-func NewOutputter() *Outputter {
-	return &Outputter{}
+func (o *outputter) die(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, "[FATAL] ")
+	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(1)
 }
 
-func sendPDU(conn net.Conn, pdu *smpp.PDU, logger *log.Logger) {
-	encoded, err := pdu.Encode()
-
-	if err != nil {
-		logger.Fatalln("Failed to encode PDU: ", err)
-	}
-
-	_, err = conn.Write(encoded)
-
-	if err != nil {
-		logger.Fatalln("Failed to write: ", err)
+func (o *outputter) debug(format string, a ...interface{}) {
+	if o.outputDebug {
+		fmt.Print("[DEBUG] ")
+		fmt.Printf(format, a...)
+		fmt.Print("\n")
 	}
 }
 
-func recvPDU(conn net.Conn, buf *[]byte, logger *log.Logger) *smpp.PDU {
-	_, err := conn.Read(*buf)
-
-	if err != nil {
-		logger.Fatalln("Failed on read: ", err)
-	}
-
-	encoded, err := smpp.DecodePDU(*buf)
-
-	if err != nil {
-		logger.Fatalln("Failed to encode incoming PDU: ", err)
-	}
-
-	return encoded
+func (o *outputter) sayThatTransportIsOpen(connectedTransport net.Conn) {
+	fmt.Printf("Transport connected from %s to %s\n", connectedTransport.LocalAddr(), connectedTransport.RemoteAddr())
 }
 
-func main() {
-	logger := log.New(os.Stderr, filepath.Base(os.Args[0])+": ", 0)
+func (o *outputter) sayThatBindWasSent() {
+	fmt.Printf("Bind was sent to remote peer\n")
+}
 
-	var peerIP string
-	var peerPort int
+func (o *outputter) sayThatBindWasReceived() {
+	fmt.Printf("Bind received from remote peer\n")
+}
 
+func (o *outputter) sayThatSubmitSmWasSent(msgNumber int) {
+	fmt.Printf("Sent SubmitSM: number %03d\n", msgNumber)
+}
+
+func (o *outputter) sayThatPduWasReceived(receivedPDU *smpp.PDU) {
+	fmt.Printf("Received PDU : %s\n", receivedPDU.CommandName())
+}
+
+type trasceiverBindInfo struct {
+	password   string
+	systemID   string
+	systemType string
+}
+
+type esmeTransceiverApp struct {
+	outputter  *outputter
+	readBuffer []byte
+	submitSm   *smpp.PDU
+}
+
+func newEsmeTransceiverApp() *esmeTransceiverApp {
+	outputter := &outputter{outputDebug: false}
+	return &esmeTransceiverApp{outputter: outputter, readBuffer: make([]byte, 9200), submitSm: generateBaseSubmitSM()}
+}
+
+func generateBaseSubmitSM() *smpp.PDU {
+	return smpp.NewPDU(smpp.CommandSubmitSm, 0, 0x5e, []*smpp.Parameter{
+		smpp.NewFLParameter(uint8(0)),                                  // service type
+		smpp.NewFLParameter(uint8(0)),                                  // source_addr_ton
+		smpp.NewFLParameter(uint8(1)),                                  // source_addr_npi
+		smpp.NewCOctetStringParameter("28809090"),                      // source_addr
+		smpp.NewFLParameter(uint8(1)),                                  // dest_addr_ton
+		smpp.NewFLParameter(uint8(1)),                                  // dest_addr_npi
+		smpp.NewCOctetStringParameter("13139591463"),                   // destination_addr
+		smpp.NewFLParameter(uint8(0)),                                  // esm_class
+		smpp.NewFLParameter(uint8(0)),                                  // protocol_id
+		smpp.NewFLParameter(uint8(0)),                                  // priority_flag
+		smpp.NewFLParameter(uint8(0)),                                  // schedule_delivery_time
+		smpp.NewFLParameter(uint8(0)),                                  // validity_period
+		smpp.NewFLParameter(uint8(0)),                                  // registered_delivery
+		smpp.NewFLParameter(uint8(0)),                                  // replace_if_flag_present
+		smpp.NewFLParameter(uint8(0xf0)),                               // data_coding
+		smpp.NewFLParameter(uint8(0)),                                  // sm_default_msg_id
+		smpp.NewFLParameter(uint8(0x8d)),                               // sm_length
+		smpp.NewOctetStringFromString("This is a test short message."), // short_message
+	}, []*smpp.Parameter{})
+}
+
+func (app *esmeTransceiverApp) processCommandLineParameters() (peerIP string, peerPort int, systemID string, messagesToSend int) {
 	flag.StringVar(&peerIP, "addr", "127.0.0.1", "IP address of peer")
 	flag.IntVar(&peerPort, "port", 2775, "Listening TCP port for peer")
+	flag.StringVar(&systemID, "id", "esme01", "ESME system-id")
+	flag.IntVar(&messagesToSend, "count", 10, "Number of messages to send")
 
 	os.Args[0] = path.Base(os.Args[0])
 
 	flag.Parse()
 
 	if peerPort < 1 || peerPort > 65535 {
-		logger.Fatalf("Port must be between 1 and 65535, inclusive")
+		app.outputter.die("Port must be between 1 and 65535, inclusive")
 	}
 
-	peerAddr := peerIP + ":" + strconv.Itoa(peerPort)
+	return peerIP, peerPort, systemID, messagesToSend
+}
 
-	logger.Println("Starting client connection to [", peerAddr, "]")
+func (app *esmeTransceiverApp) connectToSMSC(peerIP string, peerPort int) net.Conn {
+	peerAddr := peerIP + ":" + strconv.Itoa(peerPort)
 
 	conn, err := net.Dial("tcp", peerAddr)
 
 	if err != nil {
-		logger.Fatalln("Failed to establish outbound connection to [", peerAddr, "]: ", err)
+		app.outputter.die("Failed to establish outbound connection to [%s]: %s\n", peerAddr, err)
 	}
 
-	defer conn.Close()
+	return conn
+}
 
-	readbuf := make([]byte, 4096)
-
-	logger.Println("Sending bind-transmitter PDU")
-
-	bindTransmitter := smpp.NewPDU(smpp.CommandBindTransmitter, 0, 1, []*smpp.Parameter{
-		smpp.NewCOctetStringParameter("esme01"),   // system_id
-		smpp.NewCOctetStringParameter("password"), // password
-		smpp.NewCOctetStringParameter("generic"),  // system_type
-		smpp.NewFLParameter(uint8(0x34)),          // interface_version
-		smpp.NewFLParameter(uint8(0x0)),           // addr_ton
-		smpp.NewFLParameter(uint8(0x0)),           // addr_npi
-		smpp.NewCOctetStringParameter(""),         // address_range
+func (app *esmeTransceiverApp) sendBindToSMSC(transportConnection net.Conn, bindInfo trasceiverBindInfo) {
+	transceiverBindMessage := smpp.NewPDU(smpp.CommandBindTransceiver, 0, 1, []*smpp.Parameter{
+		smpp.NewCOctetStringParameter(bindInfo.systemID),
+		smpp.NewCOctetStringParameter(bindInfo.password),
+		smpp.NewCOctetStringParameter(bindInfo.systemType),
+		smpp.NewFLParameter(uint8(0x34)),  // interface_version
+		smpp.NewFLParameter(uint8(0x0)),   // addr_ton
+		smpp.NewFLParameter(uint8(0x0)),   // addr_npi
+		smpp.NewCOctetStringParameter(""), // address_range
 	}, []*smpp.Parameter{})
 
-	sendPDU(conn, bindTransmitter, logger)
+	app.sendPDU(transportConnection, transceiverBindMessage)
+}
 
-	logger.Println("Waiting for bind-transmitter-resp")
+func (app *esmeTransceiverApp) receiveBindFromSMSC(transportConnection net.Conn) {
+	_ = app.recvPDU(transportConnection)
+}
 
-	_ = recvPDU(conn, &readbuf, logger)
+func (app *esmeTransceiverApp) sendPDU(conn net.Conn, pdu *smpp.PDU) {
+	encoded, err := pdu.Encode()
 
-	logger.Println("PDU received")
+	if err != nil {
+		app.outputter.die("Failed to encode PDU: %s", err)
+	}
 
-	submitSmPDU := smpp.NewPDU(smpp.CommandSubmitSm, 0, 0x5e, []*smpp.Parameter{
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(1)),
-		smpp.NewCOctetStringParameter("28809090"),
-		smpp.NewFLParameter(uint8(1)),
-		smpp.NewFLParameter(uint8(1)),
-		smpp.NewCOctetStringParameter("13139591463"),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewCOctetStringParameter("000000000500000R"),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0xf0)),
-		smpp.NewFLParameter(uint8(0)),
-		smpp.NewFLParameter(uint8(0x8d)),
-		smpp.NewOctetStringFromString("This is a test short message, though it is somewhat longer than short, being > 50 characters! Don't get excited :@ :# :$ :% :^) emoji like..."),
-	}, []*smpp.Parameter{
-		smpp.NewTLVParameter(0x020c, uint16(5)),
-		smpp.NewTLVParameter(0x020e, uint8(2)),
-		smpp.NewTLVParameter(0x020f, uint8(1)),
-	})
+	_, err = conn.Write(encoded)
 
-	encoded, _ := submitSmPDU.Encode()
+	if err != nil {
+		app.outputter.die("Failed to write: %s", err)
+	}
+}
 
-	logger.Printf("encoded length = (%d)\n", len(encoded))
+func (app *esmeTransceiverApp) recvPDU(conn net.Conn) *smpp.PDU {
+	_, err := conn.Read(app.readBuffer)
 
-	logger.Println("Attempting to write first submit-sm PDU")
+	if err != nil {
+		app.outputter.die("Failed on read: %s", err)
+	}
 
-	sendPDU(conn, submitSmPDU, logger)
+	pdu, err := smpp.DecodePDU(app.readBuffer)
 
-	logger.Println("PDU sent; waiting response")
+	if err != nil {
+		app.outputter.die("Failed to encode incoming PDU: %s", err)
+	}
 
-	_ = recvPDU(conn, &readbuf, logger)
+	return pdu
+}
 
-	logger.Println("Received PDU")
+func (app *esmeTransceiverApp) generateSubmitSmWithFromAndMessageNumber(from string, messageNumber int) *smpp.PDU {
+	shortMsgString := fmt.Sprintf("FROM: %s | MSG NUMBER: %03d", from, messageNumber)
+	app.submitSm.MandatoryParameters[16] = smpp.NewFLParameter(uint8(len(shortMsgString)))
+	app.submitSm.MandatoryParameters[17] = smpp.NewOctetStringFromString(shortMsgString)
+	return app.submitSm
+}
 
-	logger.Println("Closing peer connection")
-	logger.Println("Done")
+func main() {
+	app := newEsmeTransceiverApp()
+
+	peerIP, peerPort, systemID, messagesToSend := app.processCommandLineParameters()
+
+	conn := app.connectToSMSC(peerIP, peerPort)
+	defer conn.Close()
+	app.outputter.sayThatTransportIsOpen(conn)
+
+	app.sendBindToSMSC(conn, trasceiverBindInfo{systemID: systemID, password: "password", systemType: "generic"})
+	app.outputter.sayThatBindWasSent()
+
+	app.receiveBindFromSMSC(conn)
+	app.outputter.sayThatBindWasReceived()
+
+	for i := 0; i < messagesToSend; i++ {
+		submitSmPDU := app.generateSubmitSmWithFromAndMessageNumber(systemID, i+1)
+		app.sendPDU(conn, submitSmPDU)
+		app.outputter.sayThatSubmitSmWasSent(i + 1)
+
+		responsePDU := app.recvPDU(conn)
+		app.outputter.sayThatPduWasReceived(responsePDU)
+	}
 }
--- examples/smsc-transceiver/smsc-transceiver-example.go
+++ examples/smsc-transceiver/smsc-transceiver-example.go
@@ -1,136 +1,221 @@
 package main
 
 import (
-    "flag"
+	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
-    "path"
-	"path/filepath"
+	"path"
 	"smpp"
-    "strconv"
+	"strconv"
 )
 
-func sendPDU(conn net.Conn, pdu *smpp.PDU, logger *log.Logger) {
+type outputter struct {
+	outputDebug bool
+}
+
+func (o *outputter) die(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, "[FATAL] ")
+	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(1)
+}
+
+func (o *outputter) debug(format string, a ...interface{}) {
+	if o.outputDebug {
+		fmt.Print("[DEBUG] ")
+		fmt.Printf(format, a...)
+		fmt.Print("\n")
+	}
+}
+
+func (o *outputter) sayThatTransportWasReceived(transportConnection net.Conn) {
+	fmt.Printf("Transport Opened From: %s\n", transportConnection.RemoteAddr().String())
+}
+
+func (o *outputter) sayThatBindWasReceived(bindPDU *smpp.PDU) {
+	fmt.Printf("transceiver-bind Received from ESME\n")
+}
+
+func (o *outputter) sayThatBindResponseWasSent() {
+	fmt.Printf("Sent transceiver-bind-resp to ESME\n\n")
+}
+
+func (o *outputter) sayThatPeerClosedTransport() {
+	fmt.Printf("Peer closed transport\n")
+}
+
+func (o *outputter) sayThatPduWasReceived(pdu *smpp.PDU) {
+	if pdu.CommandID == smpp.CommandSubmitSm {
+		fmt.Printf("Received submit-sm from ESME : Message = [%s]\n", pdu.MandatoryParameters[17].Value.([]uint8))
+	} else {
+		fmt.Printf("Received PDU from ESME       : Command = %-21.21s\n", pdu.CommandName())
+	}
+}
+
+func (o *outputter) sayThatUnexpectedPduTypeWasReceived() {
+	fmt.Printf("PDU type is unexpected.  Closing connection.\n")
+}
+
+func (o *outputter) sayThatSubmitSmResponseWasSent(systemID string, responseNumber uint32) {
+	fmt.Printf("Sent submit-sm-resp to ESME  : Id = %s-%03d\n", systemID, responseNumber)
+}
+
+func (o *outputter) sayThatWeAreWaitingForConnection(bindIP string, bindPort int) {
+	fmt.Printf("Listening for ESME connection on %s:%d\n", bindIP, bindPort)
+}
+
+type trasceiverBindResponseInfo struct {
+	systemID       string
+	sequenceNumber uint32
+}
+
+type smscTransceiverApp struct {
+	outputter  *outputter
+	readBuffer []byte
+}
+
+func newSmscTransceiverApp() *smscTransceiverApp {
+	outputter := &outputter{outputDebug: false}
+	return &smscTransceiverApp{outputter: outputter, readBuffer: make([]byte, 9200)}
+}
+
+func (app *smscTransceiverApp) processCommandLineParameters() (bindIP string, bindPort int, systemID string) {
+	flag.StringVar(&bindIP, "addr", "127.0.0.1", "IP address on which to listen for incoming connection")
+	flag.IntVar(&bindPort, "port", 2775, "Listening TCP port for incoming connection")
+	flag.StringVar(&systemID, "id", "smsc01", "SMSC system-id")
+
+	os.Args[0] = path.Base(os.Args[0])
+
+	flag.Parse()
+
+	if bindPort < 1 || bindPort > 65535 {
+		app.outputter.die("Port must be between 1 and 65535, inclusive")
+	}
+
+	return bindIP, bindPort, systemID
+}
+
+func (app *smscTransceiverApp) awaitIncomingEMSEConnection(bindIP string, bindPort int) net.Conn {
+	bindAddr := bindIP + ":" + strconv.Itoa(bindPort)
+
+	listener, err := net.Listen("tcp", bindAddr)
+
+	if err != nil {
+		app.outputter.die("Failed to start listener: %s", err)
+	}
+
+	conn, err := listener.Accept()
+
+	if err != nil {
+		app.outputter.die("Failed to Accept incoming socket: %s", err)
+	}
+
+	listener.Close()
+
+	return conn
+}
+
+func (app *smscTransceiverApp) receiveBindFromESME(fromTransport net.Conn) *smpp.PDU {
+	pdu := app.recvPDU(fromTransport)
+
+	if pdu.CommandID != smpp.CommandBindTransceiver {
+		app.outputter.die("Expected BindTransceiver from ESME, received %s", pdu.CommandName())
+	}
+
+	return pdu
+}
+
+func (app *smscTransceiverApp) sendBindResponseToESME(onTransport net.Conn, bindInfo trasceiverBindResponseInfo) {
+	transceiverBindResponseMessage := smpp.NewPDU(smpp.CommandBindTransceiverResp, 0, bindInfo.sequenceNumber, []*smpp.Parameter{
+		smpp.NewCOctetStringParameter(bindInfo.systemID),
+	}, []*smpp.Parameter{})
+
+	app.sendPDU(onTransport, transceiverBindResponseMessage)
+}
+
+func (app *smscTransceiverApp) sendPDU(conn net.Conn, pdu *smpp.PDU) {
 	encoded, err := pdu.Encode()
 
 	if err != nil {
-		logger.Fatalln("Failed to encode PDU: ", err)
+		app.outputter.die("Failed to encode PDU: %s\n", err)
 	}
 
 	_, err = conn.Write(encoded)
 
 	if err != nil {
-		logger.Fatalln("Failed to write: ", err)
+		app.outputter.die("Failed to write: %s\n", err)
 	}
 }
 
-func recvPDU(conn net.Conn, buf *[]byte, logger *log.Logger) *smpp.PDU {
-	octets, err := conn.Read(*buf)
+func (app *smscTransceiverApp) recvPDU(conn net.Conn) *smpp.PDU {
+	_, err := conn.Read(app.readBuffer)
 
 	if err != nil {
 		if err == io.EOF {
 			return nil
 		}
 
-		logger.Fatalln("Failed on read: ", err)
+		app.outputter.die("Failed on read: %s", err)
 	}
 
-	logger.Printf("Read (%d) octets", octets)
-
-	pdu, err := smpp.DecodePDU(*buf)
+	pdu, err := smpp.DecodePDU(app.readBuffer)
 
 	if err != nil {
-		logger.Fatalln("Failed to encode incoming PDU: ", err)
+		app.outputter.die("Failed to encode incoming PDU: %s", err)
 	}
 
 	return pdu
 }
 
-func main() {
-    var bindIP string
-    var bindPort int
-
-    flag.StringVar(&bindIP, "addr", "127.0.0.1", "Listener IP")
-    flag.IntVar(&bindPort, "port", 2775, "Listener TCP port")
-
-    os.Args[0] = path.Base(os.Args[0])
-
-    flag.Parse()
-
-	bindAddr := bindIP + ":" + strconv.Itoa(bindPort)
-
-	logger := log.New(os.Stderr, filepath.Base(os.Args[0])+": ", 0)
-
-	logger.Println("Starting listener")
-
-	listener, err := net.Listen("tcp", bindAddr)
-
-	if err != nil {
-		logger.Fatalln("Failed to start listener: ", err)
-	}
-
-	defer listener.Close()
-
-	conn, err := listener.Accept()
-
-	if err != nil {
-		logger.Fatalln("Failed to Accept incoming socket: ", err)
-	}
-
-	defer conn.Close()
-
-	logger.Println("Accepted incoming connection from: ", conn.RemoteAddr().String())
-
-	readbuf := make([]byte, 4096)
-
-	logger.Println("Waiting for bind-transmitter from peer")
-
-	receivedPDU := recvPDU(conn, &readbuf, logger)
-
-	if receivedPDU.CommandID != smpp.CommandBindTransmitter {
-		logger.Fatalf("Expected bind_transmitter from peer, but received (%08x)\n", receivedPDU.CommandID)
-	}
-
-	logger.Println("bind-transmitter received; sending bind-transmitter-resp")
-
-	seqNum := receivedPDU.SequenceNumber
-
-	responsePDU := smpp.NewPDU(smpp.CommandBindTransmitterResp, 0, seqNum, []*smpp.Parameter{
-		smpp.NewCOctetStringParameter("smsc01"), // system_id
+func (app *smscTransceiverApp) sendSubmitSmResponse(submitSmPDU *smpp.PDU, transportToESME net.Conn, systemID string, responseNumber uint32) {
+	respPDU := smpp.NewPDU(smpp.CommandBindTransceiverResp, 0, submitSmPDU.SequenceNumber, []*smpp.Parameter{
+		smpp.NewCOctetStringParameter(fmt.Sprintf("%s-%03d", systemID, responseNumber)),
 	}, []*smpp.Parameter{})
 
-	sendPDU(conn, responsePDU, logger)
+	app.sendPDU(transportToESME, respPDU)
+}
 
-	logger.Println("Responded with transmitter_resp")
+func (app *smscTransceiverApp) closeConnectionToESME(transport net.Conn) {
+	transport.Close()
+}
 
-	logger.Println("Entering listen loop")
+func main() {
+	app := newSmscTransceiverApp()
 
-	msgID := uint32(1)
-	for {
-		pdu := recvPDU(conn, &readbuf, logger)
+	bindIP, bindPort, systemID := app.processCommandLineParameters()
+
+	app.outputter.sayThatWeAreWaitingForConnection(bindIP, bindPort)
+
+	transport := app.awaitIncomingEMSEConnection(bindIP, bindPort)
+	defer transport.Close()
+	app.outputter.sayThatTransportWasReceived(transport)
+
+	bindPDU := app.receiveBindFromESME(transport)
+	app.outputter.sayThatBindWasReceived(bindPDU)
+
+	app.sendBindResponseToESME(transport, trasceiverBindResponseInfo{systemID: systemID, sequenceNumber: bindPDU.SequenceNumber})
+	app.outputter.sayThatBindResponseWasSent()
+
+	for responseNumber := uint32(1); true; responseNumber++ {
+		pdu := app.recvPDU(transport)
 
 		if pdu == nil {
-			logger.Println("Peer closed connection")
-			break
+			app.outputter.sayThatPeerClosedTransport()
+			os.Exit(0)
 		}
 
-		logger.Printf("Received PDU from peer, type is (%s)", pdu.CommandName())
+		app.outputter.sayThatPduWasReceived(pdu)
 
 		switch pdu.CommandID {
 		case smpp.CommandSubmitSm:
-			responsePDU = smpp.NewPDU(smpp.CommandSubmitSmResp, 0, pdu.SequenceNumber, []*smpp.Parameter{
-				smpp.NewCOctetStringParameter(fmt.Sprintf("msg-%d", msgID)),
-			}, []*smpp.Parameter{})
-			msgID++
-
-			sendPDU(conn, responsePDU, logger)
+			app.sendSubmitSmResponse(pdu, transport, systemID, responseNumber)
+			app.outputter.sayThatSubmitSmResponseWasSent(systemID, responseNumber)
 		default:
-			logger.Println("No response for this message type")
+			app.outputter.sayThatUnexpectedPduTypeWasReceived()
+			app.closeConnectionToESME(transport)
+			os.Exit(1)
 		}
 	}
-
-	logger.Println("Done")
 }
